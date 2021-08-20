b7550d238668dec612702f24dabec8fbd7ea6f9b
Alexandre Olender <olender@usp.br>
2021-08-20 09:58:34 -0300

    updating tests

--- cd_ricker_nospyro.py
+++ cd_ricker_nospyro.py
@@ -9,7 +9,7 @@ from ufl.tensors import as_vector
 import spyro
 
 ## Variables
-nx = 100
+nx = 105
 degree = 2
 L = 1.0
 mesh = fire.RectangleMesh(nx,nx,L,L)
@@ -17,15 +17,38 @@ mesh = fire.RectangleMesh(nx,nx,L,L)
 x, y = fire.SpatialCoordinate(mesh)
 c = fire.Constant(1.0)
 params = {"ksp_type": "cg", "pc_type": "jacobi"}
-dt = fire.Constant(0.0005)
+dt = fire.Constant(0.0001)
 dt_float = float(dt)
-final_time = 3.0
-nspool = 50
+final_time = 0.1
+nspool = 10
 
 ## Source information
+wavelet = spyro.full_ricker_wavelet(dt=dt_float, tf=final_time, freq=5.0)
+
+## Setting up output
+outfile = fire.File("CG_CD_MMS.pvd")
+exact_file = fire.File('exact_solution.pvd')
+output = True
+
+
+## Space
 VecFS = fire.VectorFunctionSpace(mesh,"CG", degree)
 ScaFS = fire.FunctionSpace(mesh, "CG", degree)
-V = VecFS * ScaFS
+ScaFS2 = fire.FunctionSpace(mesh, "CG", degree)
+V = VecFS * ScaFS * ScaFS2
+
+## Declaring functions
+UP = fire.Function(V)
+u, p, g = UP.split()
+
+UPn = fire.Function(V)
+un, pn, gn = UPn.split()
+
+(q_vec, q, qg) = fire.TestFunctions(V)
+
+dudt_trial, dpdt_trial, dgdt_trial = fire.TrialFunctions(V)
+
+# Source things
 wavelet = spyro.full_ricker_wavelet(dt=dt_float, tf=final_time, freq=5.0)
 model = {}
 model['opts'] = {
@@ -47,56 +70,29 @@ model['acquisition'] = {
 }
 
 comm = spyro.utils.mpi_init(model)
-source = spyro.Sources(model, mesh, ScaFS, comm)
+source = spyro.Sources(model, mesh, ScaFS2, comm)
 source.current_source = 0
 receivers = spyro.Receivers(model, mesh, ScaFS, comm)
 
-## Setting up output
-outfile = fire.File("CG_CD_MMS.pvd")
-exact_file = fire.File('exact_solution.pvd')
-output = True
-
-
-## Space
-VecFS = fire.VectorFunctionSpace(mesh,"CG", degree)
-ScaFS = fire.FunctionSpace(mesh, "CG", degree)
-V = VecFS * ScaFS
-
-## Declaring functions
-UP = fire.Function(V)
-u, p = UP.split()
-
-g = fire.Function(V.sub(1))
-
-UPn = fire.Function(V)
-un, pn = UPn.split()
-
-ss = fire.Function(V)
-g, f = ss.split()
-
-(q_vec, q) = fire.TestFunctions(V)
-
-dudt_trial, dpdt_trial = fire.TrialFunctions(V)
-
 ## Setting up equations
-LHS = dpdt_trial * q * dx + dot(dudt_trial, q_vec) * dx
-RHS = dot(u, grad(q)) * dx + p * div(q_vec) * dx
+LHS = dpdt_trial * q * dx + dot(dudt_trial, q_vec) * dx + dgdt_trial * qg * dx
+RHS = dot(u, grad(q)) * dx + p * div(q_vec) * dx + g * q * dx
 
 bcs = [fire.DirichletBC(V.sub(1), 0.0, "on_boundary")]
 
 ## Assembling matrices
 A = fire.assemble(LHS, bcs=bcs)
-B = fire.Function(V)
+
 #assembly_callable = create_assembly_callable(RHS, tensor=B)
 solv = fire.LinearSolver(A, solver_parameters=params)
+B = fire.Function(V)
 assembly_callable = create_assembly_callable(RHS, tensor=B)
 
 ## Integrating in time
 nt = int(final_time/ dt_float)  # number of timesteps
 dUP = fire.Function(V)
 
-rhs_forcing = fire.Function(V.sub(1))
-g_value = 0.0
+rhs_forcing = fire.Function(V.sub(2))
 t = 0.0
 
 for step in range(nt):
@@ -105,27 +101,25 @@ for step in range(nt):
         outfile.write(p, time=t, name="Pressure")
 
         print(f"Simulation time is: {t:{10}.{4}} seconds", flush=True)
-        print(g_value, flush = True)
     
     # Applying source
     rhs_forcing.assign(0.0)
     assembly_callable()
-    # integrating source since it is int(fdt)
-    g_value += dt_float*wavelet[step]
-    g = source.apply_source(rhs_forcing,g_value)
-
-    B1 = B.sub(1)
-    B1 += g
+    rhs_forcing = source.apply_source(rhs_forcing,wavelet[step])
+    B2 = B.sub(2)
+    B2 += rhs_forcing
 
     solv.solve(dUP, B)  # Solve for du and dp
-    dU, dP = dUP.split()
+    dU, dP, dG = dUP.split()
 
     # Updating answer
     un.assign(u + dt * dU)
     pn.assign(p + dt * dP)
+    gn.assign(g + dt * dG)
 
     u.assign(un)
     p.assign(pn)
+    g.assign(gn)
 
     
 
